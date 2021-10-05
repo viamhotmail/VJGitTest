79b3c330cdd03c60d0b88f057126bd750a533b96
vi_am <vi_am@hotmail.com>
2021-10-05 13:03:36 -0700

    changed the sout message

--- src/GitDemo.java
+++ src/GitDemo.java
@@ -1,6 +1,6 @@
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("test from GitDemo project");
-
+        System.out.println("added second line");
     }
 }
